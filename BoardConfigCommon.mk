f581f06dc3bcb2249056041f06a55f27373a94d4
Alexander Koskovich <akoskovich@pm.me>
2022-04-25 20:46:34 -0400

    universal9611: Disable unspecialized app process pool
    * Does not appear to work anymore, denials all over.
    
    Change-Id: I16e4c5bbad3a443e7b69b669f173d5869b29c633

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -137,7 +137,6 @@ TARGET_SOC := exynos9611
 include hardware/samsung_slsi-linaro/config/BoardConfig9611.mk
 
 ## Properties
-TARGET_PRODUCT_PROP += $(COMMON_PATH)/configs/prop/product.prop
 TARGET_VENDOR_PROP += $(COMMON_PATH)/configs/prop/vendor.prop
 
 ## Recovery
