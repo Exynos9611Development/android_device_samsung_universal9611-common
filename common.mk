e335fc0216dc2d4d86740cd04b151ee6cda22733
Tim Zimmermann <tim@linux4.de>
2024-02-17 15:15:33 +0100

    universal9611: Build dummy soundtrigger HAL
    * OK Google causes issues with microphone frequencies getting cut off, even with
      the prebuilt audio HAL, since we have no way over ROMs globally setting
      ro.opa.eligible_device, include a no-op soundtrigger impl so that OK Google
      is marked as unavailable (due to lack of hotword apks)
    
    Change-Id: I507b4aadd4a8c6188f6e914ae0de503b3d9397b6

--- common.mk
+++ common.mk
@@ -31,13 +31,15 @@ PRODUCT_PACKAGES += \
     android.hardware.audio@7.1-impl:32 \
     android.hardware.audio.service \
     android.hardware.bluetooth.audio-impl \
+    android.hardware.soundtrigger@2.3-impl \
     audio.bluetooth.default \
     audio.primary.exynos9611 \
     audio.r_submix.default \
     audio.usb.default \
     libtinyalsa \
     libaudioroute \
-    libtinycompress
+    libtinycompress \
+    sound_trigger.primary.dummy
 
 PRODUCT_COPY_FILES += \
     frameworks/av/services/audiopolicy/config/bluetooth_audio_policy_configuration_7_0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/bluetooth_audio_policy_configuration_7_0.xml \
