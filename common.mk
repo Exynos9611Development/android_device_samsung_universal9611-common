d681a801dc0fbb821e382748a623ff3c1260b001
Tim Zimmermann <tim@linux4.de>
2024-07-10 20:37:54 +0500

    universal9611: Switch to AIDL camera HAL
    Change-Id: Idc9ccc33c4ba9c2c6668d145405da623cf4dfc49

--- common.mk
+++ common.mk
@@ -68,7 +68,7 @@ PRODUCT_COPY_FILES += \
 
 # Camera
 PRODUCT_PACKAGES += \
-    android.hardware.camera.provider@2.5-service_64.samsung \
+    android.hardware.camera.provider-service.samsung \
     libsensorndkbridge \
     libhwjpeg
 
