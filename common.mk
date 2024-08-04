cec05d37e9abf531b1ec9a6425d69728803383e4
Tim Zimmermann <tim@linux4.de>
2024-08-04 10:56:38 +0200

    universal9611: Use pixel thermal HAL directly
    Change-Id: I30e0f13acc5f2ae4d46b4d449c49b925b930c4ca

--- common.mk
+++ common.mk
@@ -341,8 +341,8 @@ PRODUCT_SOONG_NAMESPACES += \
 
 # Thermal
 PRODUCT_PACKAGES += \
-    android.hardware.thermal-service.samsung \
-    thermal_symlinks.samsung
+    android.hardware.thermal-service.pixel \
+    thermal_symlinks
 
 PRODUCT_COPY_FILES += \
     $(COMMON_PATH)/configs/thermal/thermal_info_config.json:$(TARGET_COPY_OUT_VENDOR)/etc/thermal_info_config.json
