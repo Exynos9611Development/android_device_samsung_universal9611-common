b3a1ebbf1654b1bd72438cc10ceb0fafa271bc47
cat658011 <cat658011@gmail.com>
2026-01-07 17:47:31 +0500

    universal9611: Move to soong_config_set_bool for cbd
    error: hardware/samsung_slsi-linaro/exynos/cpboot_v3/Android.bp:12:11: module cbd variant android_vendor_arm64_armv8-a_cortex-a53: cflags: Expected all branches of a select on condition soong_config_variable(cbd, use_legacy_sipc_ioctl) to have type string, found bool

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -140,7 +140,7 @@ BOARD_USES_FULL_RECOVERY_IMAGE := true
 ## RIL
 ENABLE_VENDOR_RIL_SERVICE := true
 $(call soong_config_set,cbd,protocol,sipc)
-$(call soong_config_set,cbd,use_legacy_sipc_ioctl,true)
+$(call soong_config_set_bool,cbd,use_legacy_sipc_ioctl,true)
 
 ## Security
 VENDOR_SECURITY_PATCH := 2022-10-01
