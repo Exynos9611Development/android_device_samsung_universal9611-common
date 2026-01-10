0e6507c4e2e4e8549ef22183d84d7de127d26928
cat658011 <cat658011@gmail.com>
2026-01-10 21:39:06 +0500

    universal9611: proprietary-files: Rename libaudioroute to libaudioroute_vendor

--- extract-files.py
+++ extract-files.py
@@ -32,7 +32,6 @@ def lib_fixup_device_dep(lib: str, partition: str, *args, **kwargs):
 
 lib_fixups: lib_fixups_user_type = {
     libs_proto_3_9_1: lib_fixup_vendorcompat,
-    'libaudioroute': lib_fixup_vendor_suffix,
     'libsecnativefeature': lib_fixup_device_dep,
     'libuuid': lib_fixup_vendor_suffix,
 } # fmt: skip
@@ -56,9 +55,11 @@ blob_fixups: blob_fixups_user_type = {
         .sig_replace('80 0E 40 F9 E1 03 16 AA 82 0C 80 52 E3 03 15 AA',
             '80 0E 40 F9 E1 03 16 AA 82 0C 80 52 08 00 80 D2'),
     'vendor/lib/hw/audio.primary.exynos9611.so': blob_fixup()
+        .replace_needed('libaudioroute.so', 'libaudioroute_vendor.so')
         .add_needed('libshim_audioparams.so')
         .binary_regex_replace(b'str_parms_get_str', b'str_parms_get_mod'),
     'vendor/lib/libaudioproxy.so': blob_fixup()
+        .replace_needed('libaudioroute.so', 'libaudioroute_vendor.so')
         .add_needed('libshim_mixerparams.so'),
     'vendor/lib64/libvkservice.so': blob_fixup()
         .binary_regex_replace(b'ro.factory.factory_binary', b'ro.vendor.factory_binary\x00'),
