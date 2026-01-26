0f86cee9ab5957b737fb42f17124d2b5c7e68ce2
cat658011 <cat658011@gmail.com>
2026-01-26 21:24:02 +0500

    universal9611: extract-files: Drop lib_fixup_device_dep for libsecnativefeature
    Not need anymore

--- extract-files.py
+++ extract-files.py
@@ -27,12 +27,8 @@ namespace_imports = [
 def lib_fixup_vendor_suffix(lib: str, partition: str, *args, **kwargs):
     return f'{lib}_{partition}' if partition == 'vendor' else None
 
-def lib_fixup_device_dep(lib: str, partition: str, *args, **kwargs):
-    return f'//device/samsung/universal9611-common/shims/stub:{lib}'
-
 lib_fixups: lib_fixups_user_type = {
     libs_proto_3_9_1: lib_fixup_vendorcompat,
-    'libsecnativefeature': lib_fixup_device_dep,
     'libuuid': lib_fixup_vendor_suffix,
 } # fmt: skip
 
