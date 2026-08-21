93d00da0a15fa05d6beb620db1dce9983c856bfd
cat658011 <cat658011@gmail.com>
2026-08-21 23:54:57 +0500

    Revert "universal9611: proprietary-files: Rename libaudioroute to libaudioroute_vendor"
    This reverts commit 0e6507c4e2e4e8549ef22183d84d7de127d26928.

--- extract-files.py
+++ extract-files.py
@@ -51,11 +51,9 @@ blob_fixups: blob_fixups_user_type = {
         .sig_replace('80 0E 40 F9 E1 03 16 AA 82 0C 80 52 E3 03 15 AA',
             '80 0E 40 F9 E1 03 16 AA 82 0C 80 52 08 00 80 D2'),
     'vendor/lib/hw/audio.primary.exynos9611.so': blob_fixup()
-        .replace_needed('libaudioroute.so', 'libaudioroute_vendor.so')
         .add_needed('libshim_audioparams.so')
         .binary_regex_replace(b'str_parms_get_str', b'str_parms_get_mod'),
     'vendor/lib/libaudioproxy.so': blob_fixup()
-        .replace_needed('libaudioroute.so', 'libaudioroute_vendor.so')
         .add_needed('libshim_mixerparams.so'),
     'vendor/lib64/libvkservice.so': blob_fixup()
         .binary_regex_replace(b'ro.factory.factory_binary', b'ro.vendor.factory_binary\x00'),
